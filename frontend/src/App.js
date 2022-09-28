933b9823551fdf1b850f84b38e410bc7e8022487
Abhishek Sukumar <abhishekskmr44@gmail.com>
2022-09-28 21:06:39 +0530

    day1

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const App = () => {
+
+
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem('colorMode');
+    const currentThemeMode = localStorage.getItem('themeMode');
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
+  }, []);
+
+  return (
+    <div>App</div>
+  )
+}
