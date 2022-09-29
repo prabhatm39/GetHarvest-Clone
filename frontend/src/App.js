70ef625e64906ee6299d1e13c71f61d59b453c3a
Abhishek Sukumar <abhishekskmr44@gmail.com>
2022-09-29 10:07:25 +0530

    ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 export const App = () => {
 
-
   useEffect(() => {
     const currentThemeColor = localStorage.getItem('colorMode');
+
     const currentThemeMode = localStorage.getItem('themeMode');
+
     if (currentThemeColor && currentThemeMode) {
       setCurrentColor(currentThemeColor);
       setCurrentMode(currentThemeMode);
+
     }
   }, []);
 
