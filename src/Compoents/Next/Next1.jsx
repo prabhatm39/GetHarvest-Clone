59f6eba518a28f38f41ecdb74cad0677456591e0
Sukanta Dolai <101576636+Sukanta1304@users.noreply.github.com>
2022-10-01 08:45:46 +0530

    complete signup, login and manage page

---
+++ src/Compoents/Next/Next1.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import "./NextStyles.css";
+
+export default function Next1() {
+  const navigate = useNavigate();
+  const [team, setTeam] = useState("");
+  const location = useLocation();
+  let { email } = location.state;
+  console.log(email);
+  //console.log(team)
+  const next = () => {
+    axios
+      .put(`http://localhost:8080/user/edit/${email}`, { team })
+      .then((res) => {
+        console.log(res);
+        navigate("/last", { state: { email: email } });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      <div className="nextmain">
+        <h1>Welcome to Harvest</h1>
+        <h4>We just have a few quick questions to set up your account…</h4>
+        <br />
+        <h5>First, about how many people will be using Harvest?</h5>
+        <select name="teamsize" id="" onChange={(e) => setTeam(e.target.value)}>
+          <option value="default1">Select a team size</option>
+          <option value="only me">Only me</option>
+          <option value="2">2</option>
+          <option value="3-9">3-9</option>
+          <option value="10-19">10-19</option>
+          <option value="20+">20+</option>
+        </select>
+        <br />
+        <br />
+        <button onClick={next}>Next</button>
+      </div>
+    </div>
+  );
+}
